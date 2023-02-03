201b050f8446ef7831e7912cc0cdf3d2c8fdc9e0
Mike Esposito <mikeesposito6852@gmail.com>
2023-02-03 04:13:29 -0800

    Write capitalize function and tests

--- capitalize.test.js
+++ capitalize.test.js
@@ -3,3 +3,11 @@ const capitalize = require('./capitalize');
 test('capitalizes the string "hello"', () => {
   expect(capitalize("hello")).toBe("Hello");
 });
+
+test('capitalizes the string "gOOdByE"', () => {
+  expect(capitalize("gOOdByE")).toBe("Goodbye");
+});
+
+test('does not return the string "HELLO" when given the string "hello"', () => {
+  expect(capitalize("hello")).not.toBe("HELLO");
+});
