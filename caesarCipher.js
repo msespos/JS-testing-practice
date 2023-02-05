bb50f765de81437e830dc33c2b14c536199f712e
Mike Esposito <mikeesposito6852@gmail.com>
2023-02-05 07:15:14 -0800

    Refactor to DRY up code

--- caesarCipher.js
+++ caesarCipher.js
@@ -1,21 +1,11 @@
 const caesarCipher = (string, shift) => {
   return string.split('').map((char) => {
-    if (shift >= 0) {
-      if (char.charCodeAt() >= 97 && char.charCodeAt() <= 122) {
-        return String.fromCharCode((char.charCodeAt() - 97 + shift) % 26 + 97);
-      } else if (char.charCodeAt() >= 65 && char.charCodeAt() <= 90) {
-        return String.fromCharCode((char.charCodeAt() - 65 + shift) % 26 + 65);
-      } else {
-        return char
-      } 
+    if (char.charCodeAt() >= 97 && char.charCodeAt() <= 122) {
+      return String.fromCharCode((char.charCodeAt() - 97 + (26 + shift % 26)) % 26 + 97);
+    } else if (char.charCodeAt() >= 65 && char.charCodeAt() <= 90) {
+      return String.fromCharCode((char.charCodeAt() - 65 + (26 + shift % 26)) % 26 + 65);
     } else {
-      if (char.charCodeAt() >= 97 && char.charCodeAt() <= 122) {
-        return String.fromCharCode((char.charCodeAt() - 97 + (26 + shift % 26)) % 26 + 97);
-      } else if (char.charCodeAt() >= 65 && char.charCodeAt() <= 90) {
-        return String.fromCharCode((char.charCodeAt() - 65 + (26 + shift % 26)) % 26 + 65);
-      } else {
       return char
-      }
     }
   }).join('');
 }
